7a065a7dc596c5ce8accc0e47b7fc44f2c1f0f42
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-24 09:52:40 -0500

    created crud methods in service class: add empl, find , emp, update and delete emp

--- employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -2,9 +2,11 @@ package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,5 +24,26 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
 
+    // list of employees
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    // UPDATE EMP
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    //find employee by id
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(()->new UserNotFoundException("User by id "+id+" was not Found"));
+    }
+
+
+    //DELETE EMP
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
 
 }
