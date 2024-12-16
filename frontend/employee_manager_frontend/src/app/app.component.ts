1323a3de656b000c0398d5a37daef25451f16bc5
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-12-16 06:11:11 -0500

    updated app components, used employee component here

--- frontend/employee_manager_frontend/src/app/app.component.ts
+++ frontend/employee_manager_frontend/src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { response } from 'express';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  // standalone: true,
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'employee_manager_frontend';
+  public employees: Employee[]; 
+  
+  constructor(private employeeService: EmployeeService){}
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees=response;
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message)
+      }
+
+    )
+  }
+
+
+
 }
