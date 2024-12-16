e9513f345b624f71aacbbdcfd39b5450b4cda1a9
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-12-16 06:10:42 -0500

    created an employee service

---
+++ frontend/employee_manager_frontend/src/app/employee.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  // get employee
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  // add employee
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(
+      `${this.apiServerUrl}/employee/add`,
+      employee
+    );
+  }
+
+  // update employee
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(
+      `${this.apiServerUrl}/employee/update`,
+      employee
+    );
+  }
+
+  // delete employee
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+}
